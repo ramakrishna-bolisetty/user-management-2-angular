70e097a4d75c4172404818758494440e382c8ada
ramakrishna-bolisetty <ramakrishna_bolisetty@epam.com>
2023-05-15 11:04:20 +0530

    updated

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const usersURL:string = "http://localhost:3000/users"
-export const lastId:number = 20;
-export const DETAILS:string='Details';
-export const ACTIVATE:string='Activate';
-export const DEACTIVATE:string='Deactivate';
+export const usersURL: string = "http://localhost:3000/users"
+export const lastId: number = 20;
+export const DETAILS: string = 'Details';
+export const ACTIVATE: string = 'Activate';
+export const DEACTIVATE: string = 'Deactivate';
